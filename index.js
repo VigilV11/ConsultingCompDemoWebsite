3f56f5a8be5f34c92e64bc100532fc90f48e2acd
VigilV11 <vigilv@pm.me>
2021-03-23 20:57:38 +0530

    add intersection observer to make navbar transparent when at the top of the page

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-const canvas = document.getElementById('canv');
-const ctx = canvas.getContext('2d');
+// Displays a matrix screen
+// const canvas = document.getElementById('canv');
+// const ctx = canvas.getContext('2d');
 
-const w = (canvas.width = document.body.offsetWidth);
-const h = (canvas.height = document.body.offsetHeight);
-const cols = Math.floor(w / 20) + 1;
-const ypos = Array(cols).fill(0);
+// const w = (canvas.width = document.body.offsetWidth);
+// const h = (canvas.height = document.body.offsetHeight);
+// const cols = Math.floor(w / 20) + 1;
+// const ypos = Array(cols).fill(0);
 
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, w, h);
+// ctx.fillStyle = '#000';
+// ctx.fillRect(0, 0, w, h);
 
-function matrix() {
-  ctx.fillStyle = '#0001';
-  ctx.fillRect(0, 0, w, h);
+// function matrix() {
+//   ctx.fillStyle = '#0001';
+//   ctx.fillRect(0, 0, w, h);
 
-  ctx.fillStyle = '#0f0';
-  ctx.font = '15pt monospace';
+//   ctx.fillStyle = '#0f0';
+//   ctx.font = '15pt monospace';
 
-  ypos.forEach((y, ind) => {
-    const text = String.fromCharCode(Math.random() * 128);
-    const x = ind * 20;
-    ctx.fillText(text, x, y);
-    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
-    else ypos[ind] = y + 20;
-  });
-}
+//   ypos.forEach((y, ind) => {
+//     const text = String.fromCharCode(Math.random() * 128);
+//     const x = ind * 20;
+//     ctx.fillText(text, x, y);
+//     if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+//     else ypos[ind] = y + 20;
+//   });
+// }
 
-setInterval(matrix, 50);
+// setInterval(matrix, 50);
+
+const heroSection = document.querySelector('.hero');
+const navbar = document.getElementById('navbar');
+
+const obsFunc = function (entries, observer) {
+  const [entry] = entries;
+
+  // If "hero" section intersects the root add the "top" class, otherwise remove it
+  if (entry.isIntersecting) {
+    navbar.classList.add('top');
+  } else {
+    navbar.classList.remove('top');
+  }
+};
+
+const obsOptions = {
+  root: null,
+  rootMargin: '-70px',
+  threshold: 0,
+};
+const observer = new IntersectionObserver(obsFunc, obsOptions);
+observer.observe(heroSection);
